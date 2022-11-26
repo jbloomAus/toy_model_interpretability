fd62f901112abb56a14df4bb713a6b3bbce4484d
jbloom-md <joseph@massdynamics.com>
2022-11-26 10:50:10 +0000

    find good default values

--- run_toy_models.sh
+++ run_toy_models.sh
@@ -1,10 +1,10 @@
 python3 -m adam_jermyn.model3 \
-    --k 128 \
-    --log2_batch_size 6 \
-    --log2_training_steps 10 \
-    --learning_rate 0.01 \
+    --k 1024 \
+    --log2_batch_size 7 \
+    --log2_training_steps 12 \
+    --learning_rate 0.03 \
     --sample_kind equal \
-    --init_bias -1 \
+    --init_bias 0 \
     --nonlinearity ReLU \
     --task autoencoder \
     --decay 0 \
