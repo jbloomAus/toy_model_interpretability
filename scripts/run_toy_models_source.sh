dc4af891e1639940e1ce1600ade56b8440b652e3
jbloom-md <joseph@massdynamics.com>
2022-11-26 16:37:44 +0000

    move scripts

--- scripts/run_toy_models_source.sh
+++ scripts/run_toy_models_source.sh
@@ -1,11 +1,11 @@
 python3 -m main \
-    --N 512 \
-    --m 64 \
-    --k 1024 \
+    --N 256 \
+    --m 32 \
+    --k 612 \
     --eps 0.015625 \
-    --log2_batch_size 7 \
+    --log2_batch_size 6 \
     --learning_rate 0.03 \
-    --log2_training_steps 17 \
+    --log2_training_steps 13 \
     --sample_kind equal \
     --task autoencoder \
     --decay 0 \
@@ -13,6 +13,7 @@ python3 -m main \
     --nonlinearity ReLU \
     --reg 0 \
     --sweep_var learning_rate \
-    --sweep_values 0.001 0.003 0.005 0.007 0.01 0.03\
+    --sweep_values 0.001 0.003 \
     --file_name LR1_reduced \
+    --device cpu 
 
