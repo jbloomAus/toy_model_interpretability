a9fe4a0ce6af240cd0d23ff5af01cd3def45c21c
jbloom-md <joseph@massdynamics.com>
2022-11-26 11:13:02 +0000

    refactor run to be nicer

--- source/run.py
+++ source/run.py
@@ -1,19 +1,20 @@
 import torch
 import torch.nn as nn
 from train import train
-from activations import SoLU
+
+from utils import get_activation_from_string, get_sampling_function_from_string, get_output_embedder, get_output_dim
 from models import get_model
 from sampling import sample_vectors_equal, sample_vectors_power_law, make_random_embedder
 
 def train_model(N,m,k,eps,batch_size,learning_rate,training_steps,sample_kind,init_bias,nonlinearity,task,decay,reg, device = 'cpu'):
-    if sample_kind == 'equal':
-        sampler = sample_vectors_equal
-    elif sample_kind == 'power_law':
-        sampler = sample_vectors_power_law
-    else:
-        print('Sample kind not recognized. Exiting.')
-        exit()
+    
+    # parse arguments
+    sampler = get_sampling_function_from_string(sample_kind)
+    activation = get_activation_from_string(nonlinearity)
+    output_embedder = get_output_embedder(task, N, m)
+    output_dim = get_output_dim(task, N, m)
 
+    # store config
     setup = {
         'N':N,
         'm':m,
@@ -21,36 +22,28 @@ def train_model(N,m,k,eps,batch_size,learning_rate,training_steps,sample_kind,in
         'batch_size':batch_size,
         'learning_rate':learning_rate,
         'eps':eps,
-        'fixed_embedder': make_random_embedder(N,m),
+        'fixed_embedder': make_random_embedder(N,m), # I need to think about this
         'sampler':sampler,
         'task': task,
         'decay': decay,
-        'reg': reg
+        'reg': reg,
+        'output_embedder': output_embedder,
+        'output_dim': output_dim,
+        'activation': activation,
+        'init_bias': init_bias
     }
 
-    if task == 'random_proj':
-        setup['output_embedder'] = make_random_embedder(N,m)
-        
-    if nonlinearity == 'ReLU':
-        activation = nn.ReLU()
-    elif nonlinearity == 'GeLU':
-        activation = nn.GELU()
-    elif nonlinearity == 'SoLU':
-        activation = SoLU()
-    else:
-        print('No valid activation specified. Quitting.')
-        exit()
-
-    if task == 'random_proj':
-        output_dim = m
-    else:
-        output_dim = N
-
+    # get model, set bias
     model = get_model(m, k, output_dim, activation, device)
+    model = set_bias_mean(model, init_bias)
 
-    state = model.state_dict()
-    state['0.bias'] += init_bias
-    model.load_state_dict(state)
-                
+    # train model
     losses, model, models = train(setup, model, training_steps)
+
     return losses, model, models, setup
+
+def set_bias_mean(model, mean):
+    state = model.state_dict()
+    state['0.bias'] += mean
+    model.load_state_dict(state)
+    return model
---
+++ source/utils.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+from activations import SoLU
+from sampling import sample_vectors_equal, sample_vectors_power_law, make_random_embedder
+
+def get_activation_from_string(activation_string):
+    if activation_string == 'ReLU':
+        return nn.ReLU()
+    elif activation_string == 'GeLU':
+        return nn.GELU()
+    elif activation_string == 'SoLU':
+        return SoLU()
+    else:
+        raise ValueError('No valid activation specified. Quitting.')
+
+def get_sampling_function_from_string(sampling_string):
+    if sampling_string == 'equal':
+        return sample_vectors_equal
+    elif sampling_string == 'power_law':
+        return sample_vectors_power_law
+    else:
+        raise ValueError('No valid sampling function specified. Quitting.')
+    
+def get_output_embedder(task, N, m):
+    if task == 'random_proj':
+        return make_random_embedder(N,m)
+    else: 
+        return None
+    
+def get_output_dim(task, N, m):
+    if task == 'random_proj':
+        return m
+    else:
+        return N
