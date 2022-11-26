59ec591c3c8b99a161f501babfb2ecd2664762ea
jbloom-md <joseph@massdynamics.com>
2022-11-26 11:56:49 +0000

    reorder args in script

--- run_toy_models.sh
+++ run_toy_models.sh
@@ -1,16 +1,16 @@
 python3 -m adam_jermyn.model3 \
+    --N 512 \
+    --m 64 \
     --k 1024 \
+    --eps 0.015625 \
     --log2_batch_size 7 \
-    --log2_training_steps 12 \
     --learning_rate 0.03 \
+    --log2_training_steps 12 \
     --sample_kind equal \
-    --init_bias 0 \
-    --nonlinearity ReLU \
     --task autoencoder \
     --decay 0 \
-    --eps 0.015625 \
-    --m 64 \
-    --N 512 \
+    --init_bias 0 \
+    --nonlinearity ReLU \
     --reg 0 \
 
 
